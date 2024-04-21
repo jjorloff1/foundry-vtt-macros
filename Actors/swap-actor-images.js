40a776aad9cf0ac4cb0ab433f429a5b94f77328f
name <email>
2024-04-21 17:05:45 -0500

    Updated swap-actor-images to use selected tokens.

--- Actors/swap-actor-images.js
+++ Actors/swap-actor-images.js
@@ -1,16 +1,32 @@
 main();
 
+/**
+ * This macro swaps the images of 2 actors and their selected tokens.
+ * Select two tokens representing two different actors and it will swap the images.
+ *
+ * @returns {Promise<void>}
+ */
 async function main() {
-    let actor1 = game.actors.get("6mlkshrCyVoPyivp");
-    let actor2 = game.actors.get("36iciDAsYfgKkSK3");
+    // Get 2 selected tokens
+    if (canvas.tokens.controlled.length != 2) {
+        ui.notifications.error("You must select two tokens.");
+        return;
+    }
+    let token1 = canvas.tokens.controlled[0];
+    let token2 = canvas.tokens.controlled[1];
+
+    let actor1 = game.actors.get(token1.actor.id);
+    let actor2 = game.actors.get(token2.actor.id);
 
     let actor1Image = actor1.img;
     console.log(actor1Image);
     let actor2Image = actor2.img;
     console.log(actor2Image);
 
+    token1.document.update({"img": actor2Image});
     actor1.update({"img": actor2Image});
-    console.log("updated actor1")
+    console.log("updated token1 and actor1")
+    token2.document.update({"img": actor1Image});
     actor2.update({"img": actor1Image});
-    console.log("updated actor2")
+    console.log("updated token2 and actor2");
 }
